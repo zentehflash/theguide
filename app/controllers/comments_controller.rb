699f0d081592a212dfbec51a3b699795736ac7be
Robert Robinson <tehflash@gmail.com>
2012-08-26 19:27:07 -0400

    Finished adding partials for comments and a delete button.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,17 @@
 
 class CommentsController < ApplicationController
+	#POST
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(params[:comment])
+		redirect_to post_path(@post)
+	end
+
+	#DELETE
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
 		redirect_to post_path(@post)
 	end
 end
